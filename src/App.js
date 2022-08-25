cf7cdd1d30a6de931cf5d86589a876688b340056
trav15 <travistokuyama@gmail.com>
2022-08-25 08:28:11 +0100

    Add Private Route for user profile page, and loading spinner

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
           <Routes>
             <Route path='/' element={<Explore />} />
             <Route path='/offers' element={<Offers />} />
-            <Route path='/profile' element={<Profile />} />
+            <Route path='/profile' element={<PrivateRoute />}>
+              <Route path='/profile' element={<Profile />} />
+            </Route>
             <Route path='/sign-in' element={<SignIn />} />
             <Route path='/sign-up' element={<SignUp />} />
             <Route path='/forgot-password' element={<ForgotPassword />} />
