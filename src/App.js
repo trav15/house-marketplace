af11d0f789cf638d194177950c0a67ce2a4a23fc
trav15 <travistokuyama@gmail.com>
2022-09-02 08:57:46 +0100

    Create Category component and add route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
+import Category from './pages/Category';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Explore />} />
             <Route path='/offers' element={<Offers />} />
+            <Route path='/category/:categoryName' element={<Category />} />
             <Route path='/profile' element={<PrivateRoute />}>
               <Route path='/profile' element={<Profile />} />
             </Route>
