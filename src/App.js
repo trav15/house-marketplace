25abd294520b6b517b1656effbd2f99b1a070e0b
trav15 <travistokuyama@gmail.com>
2022-09-02 11:31:02 +0100

    Create CreateListing page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import Category from './pages/Category';
+import CreateListing from './pages/CreateListing';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
             <Route path='/sign-in' element={<SignIn />} />
             <Route path='/sign-up' element={<SignUp />} />
             <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path='create-listing' element={<CreateListing />} />
           </Routes>
           <Navbar />
       </Router>
---
+++ src/pages/CreateListing.jsx
@@ -0,0 +1,55 @@
+import { useState, useRef, useEffect } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+
+function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false)
+  const [formData, setFormData] = useState({
+      type: 'rent',
+      name: '',
+      bedrooms: 1,
+      bathrooms: 1,
+      parking: false,
+      furnised: false,
+      address: '',
+      offer: false,
+      regularPrice: 0,
+      discountedPrice: 0,
+      images: {},
+      latitude: 0,
+      longitude: 0 
+  });
+
+  const auth =  getAuth()
+  const navigate = useNavigate()
+  const isMounted = useRef(true)
+
+  useEffect(() => {
+      if(isMounted) {
+        onAuthStateChanged(auth, (user) => {
+          if(user) {
+            setFormData({...formData, userRef: user.uid})
+          } else {
+            navigate('/sign-in')
+          }
+        })
+      }
+
+      return () => {
+          isMounted.current = false;
+      }
+  }, [isMounted])
+
+  if(loading) {
+    return <Spinner />
+  }
+
+
+  return (
+    <div>CreateListing</div>
+  )
+}
+
+export default CreateListing
